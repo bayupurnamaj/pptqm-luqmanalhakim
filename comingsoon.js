020b63a1d5100aa69c92dfa31ea157e27f953b0d
bayupurnamaj <belajarweb220@gmail.com>
2023-03-30 14:42:47 +0700

    ganti img| comingsoon page|

---
+++ comingsoon.js
@@ -0,0 +1,18 @@
+document.getElementById("location").addEventListener("click", function () {
+  window.open(
+    "https://www.google.com/maps/place/PPTQM+Luqman+Al-Hakim+Semarang/@-7.0689947,110.3956374,15z/data=!4m6!3m5!1s0x2e70896f45a65d3d:0xf79fb31854f98939!8m2!3d-7.0689947!4d110.3956374!16s%2Fg%2F11fzf2_xq1",
+    "_blank"
+  );
+});
+
+document.getElementById("number").addEventListener("click", function () {
+  window.open("https://wa.me/+628157109082", "_blank");
+});
+
+document.getElementById("gmail").addEventListener("click", function () {
+  window.open("mailto:pptqmlhpatemon@gmail.com", "_blank");
+});
+
+document.getElementById("daftar").addEventListener("click", function () {
+  window.open("daftar.html", "_blank");
+});
